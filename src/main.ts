b6cc32c4b94ed2b99070772f3af7e31ac807f9c0
BlueBiuBiu <1910916141@qq.com>
2023-06-06 14:26:49 +0800

    feat: 增加跨域跟限速（免受暴力攻击）

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  /**
+   * 全局管道验证
+   */
   app.useGlobalPipes(new ValidationPipe());
+
+  /**
+   * 全局异常拦截器
+   */
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  /**
+   * 限速（保护您的应用程序免受暴力攻击）
+   */
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
